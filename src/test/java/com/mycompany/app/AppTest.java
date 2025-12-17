7705be71b8a23a35fea85e58c2eeaa84f4157365
vincev20 <vincev20@yahoo.com>
2025-12-17 16:13:59 +0800

    apptest

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/** Unit test for simple App. */
+/** Unit test for simple App2. */
 class AppTest {
 
   @Test
